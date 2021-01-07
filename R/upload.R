36e9c798b1281baf5c9bc506c9d90d87a1cc4781
pdelboca <patriciodelboca@gmail.com>
2021-01-07 10:04:18 -0300

    Add extra headers parameter to batch function

--- R/upload.R
+++ R/upload.R
@@ -1,13 +1,13 @@
 #' Upload a file to LFS storage
 #'
 #' @return list with sha256 and size of the object
-lfs_upload <- function(file_path, repo, dataset, token, transfers=c("multipart-basic", "basic")) {
+lfs_upload <- function(file_path, repo, dataset, token, transfers=c("multipart-basic", "basic"), headers = c()) {
   hash <- digest(file=file_path, algo='sha256')
   size <- file.size(file_path)
   object <- list(list(oid = hash, size = size))
   prefix <- paste0(repo, '/', dataset)
 
-  resp <- batch(prefix, object, token, transfers)
+  resp <- batch(prefix, object, token, transfers, headers = headers)
 
   upload_specs <- resp$objects[[1]]
   if(!'actions' %in% names(upload_specs)){
